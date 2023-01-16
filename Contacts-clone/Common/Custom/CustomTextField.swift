ddc1e9d552187503efa6d171ee156b38504c8d51
김영균 <kyoungyun98@naver.com>
2023-01-16 22:55:10 +0900

    custom textfield

---
+++ Contacts-clone/Common/Custom/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  Contacts-clone
+//
+//  Created by 김영균 on 2023/01/16.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
